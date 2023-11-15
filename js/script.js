1076c30db464f8f97e61a975a5793c6fbfda1307
gqdThinky <gqdThinky@gmail.com>
2023-11-15 14:52:12 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var svg1 = document.getElementById("svg-1");
+    var svg2 = document.getElementById("svg-2");
+    var svg3 = document.getElementById("svg-3");
+
+    svg1.addEventListener('click', function () {
+        confirm('ℹ️> Add on Discord:\n🔥>  gqdThinky')
+    });
+    svg2.addEventListener('click', function () {
+        window.open('https://github.com/gqdThinky', '_blank');
+    });
+    svg3.addEventListener('click', function () {
+        window.open('mailto:gqdThinky@gmail.com', '_blank');
+    });
+});
